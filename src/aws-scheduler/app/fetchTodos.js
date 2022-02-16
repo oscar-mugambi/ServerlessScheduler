34b209b3c38927191fe7114841a26e52e75e6583
oscar-mugambi <oscarmugambi@gmail.com>
2022-02-16 23:48:30 +0300

    minor fix

---
+++ src/aws-scheduler/app/fetchTodos.js
@@ -0,0 +1,20 @@
+// 'use strict';
+// const AWS = require('aws-sdk');
+
+// const fetchTodo = async (event) => {
+//   const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+//   dynamodb.put({
+//     TableName: 'TodoTable',
+//     Item: newTodo,
+//   });
+
+//   return {
+//     statusCode: 200,
+//     body: JSON.stringify(newTodo),
+//   };
+// };
+
+// module.exports = {
+//   handler: fetchTodo,
+// };
