974f4944f93707f993ca344e14dfbf99b5b9e0d2
oscar-mugambi <oscarmugambi@gmail.com>
2022-02-16 21:55:56 +0300

    added addTodo

---
+++ src/aws-scheduler/app/getConversation.js
@@ -0,0 +1,33 @@
+'use strict';
+const { v4 } = require('uuid');
+const AWS = require('aws-sdk');
+
+const addTodo = async (event) => {
+  const dynamodb = new AWS.DynamoDB.DocumentClient();
+  const { todo } = JSON.parse(event.body);
+  const createdAt = new Date().toISOString();
+  const id = v4();
+
+  console.log('This is an id ' + id);
+
+  const newTodo = {
+    id,
+    todo,
+    createdAt,
+    competed: false,
+  };
+
+  dynamodb.put({
+    TableName: 'TodoTable',
+    Item: newTodo,
+  });
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(newTodo),
+  };
+};
+
+module.exports = {
+  handler: addTodo,
+};
